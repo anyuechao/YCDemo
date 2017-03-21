fc2205120288bde6ee7cc137e1eba0036a91a592
yuechao An <djnet@DJnetdeiMac.local>
2017-03-21 16:01:38 +0800

    Initial Commit

---
+++ YCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YCDemo
+//
+//  Created by DJnet on 2017/3/21.
+//  Copyright © 2017年 YueChao An. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YCDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YCDemo
+//
+//  Created by DJnet on 2017/3/21.
+//  Copyright © 2017年 YueChao An. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
